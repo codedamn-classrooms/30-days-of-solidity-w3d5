23acbc565a146f8db6f6d4ceed723890bbd832ef
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-04-19 13:41:42 +0530

    initial

---
+++ contracts/Day5.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day5 {
+    function search(
+        int256[] memory arr,
+        uint256 size,
+        int256 toSearch
+    ) public pure returns (uint256) {
+        for (uint256 i = 0; i < size; i++) {
+            if (arr[i] == toSearch) {
+                return 1; //if element is found
+            }
+        }
+        return 0; //if element is not found
+    }
+}
